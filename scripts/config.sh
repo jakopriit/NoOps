75f6e4850f024c7ff344b63dcc8409d97acdb587
jako <jako-priit.raud@eesti.ee>
2023-08-29 10:16:28 +0300

    Update config.sh
    sane defaults

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-organization=ORGANISATION
+organization= #.ee TLD subdomain
 username=ubuntu
-safe_tim_db=PASSWORD(min 6 characters)
-safe_byk_db=PASSWORD(min 6 characters)
+safe_tim_db=PASSWORD #(min 6 characters)
+safe_byk_db=PASSWORD #(min 6 characters)
 buildpath="$(pwd)"
-timdb=DATABASE-VM-IP
-taraid=GIVEN-FROM-TARA-ENVELOPE
-tarapass=GIVEN-FROM-TARA-ENVELOPE
-keytoolpass=PASSWORD(min 6 characters)
-bot_url=BOT-VM-IP
-training_url=TRAININGBOT-VM-IP
+timdb=databases #DATABASE-VM-IP
+taraid= #GIVEN-FROM-TARA-ENVELOPE
+tarapass= #GIVEN-FROM-TARA-ENVELOPE
+keytoolpass=PASSWORD #(min 6 characters)
+bot_url=bot #BOT-VM-IP
+training_url=trainingbot #TRAININGBOT-VM-IP
 training_bot_dir=chatbot
-SSH_KEY=PATH-TO-SSH-KEY(id_rsa)
-BOT_DATA_DIR=PATH-TO-BOT-DATA
-bot_name=ORGANISATION-NAME-FOR-BOT
+SSH_KEY=/home/ubuntu/.ssh/id_rsa #PATH-TO-SSH-KEY(id_rsa)
+BOT_DATA_DIR=/home/bot-data #PATH-TO-BOT-DATA
+bot_name=kratt #ORGANISATION-NAME-FOR-BOT
